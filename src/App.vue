c1c1cd088c49e5f3aa8892bfc0c1627dc1784592
郭云亮 <1060950677@qq.com>
2020-05-12 09:42:06 +0800

    axios + jsonp

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+// import axios from 'axios'
+import jsonp from 'jsonp'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      data: ''
+    }
+  }, 
+  mounted() {
+    let url = "https://www.imooc.com/activity/servicetime";
+    // axios.get(url).then(() => {
+
+    // });
+
+    jsonp(url, (err, res) => {
+      let result = res;
+      this.data = result;
+    })
+  }
+}
+</script>
 
 <style>
 #app {
@@ -15,18 +41,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 60px;
 }
 </style>
