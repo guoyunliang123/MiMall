c1ae11fe69c3b025175ecc28489587db23598c0e
郭云亮 <1060950677@qq.com>
2020-05-15 16:38:55 +0800

    接口错误拦截封装

--- src/main.js
+++ src/main.js
@@ -1,7 +1,28 @@
 import Vue from 'vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import App from './App.vue'
 
+// 根据前端的跨域方式进行调整
+axios.defaults.baseURL = '/api';
+// 超时时间  8 秒
+axios.defaults.timeout = 8000;
+// 接口错误拦截
+axios.interceptors.response.user(function(response) {
+  // status = 1 正常   = 10 未登录
+  let res = response.data;
+  if(res.status == 0) {
+    return res.data;
+  } else if(res.status == 10) {
+    window.location.href = '/#/login';
+  } else {
+    alert(res.msg);
+  }
+});
+
+Vue.use(VueAxios, axios);
 Vue.config.productionTip = false
 
 new Vue({
