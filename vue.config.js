6dd27cce63cbb363ded0ed00824dd2c642d1bd95
郭云亮 <1060950677@qq.com>
2020-05-22 15:59:57 +0800

    navbar 头部组件   接口调用

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
     // }
     proxy: {
       '/api': {
-        target: 'https://www.imooc.com',
+        target: 'http://mall-pre.springboot.cn',
         changeOrigin: true,
         pathRewrite: {
           '/api': ''
