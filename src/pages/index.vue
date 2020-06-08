ae7217550f4726e538b9581bd9c738f29760c4f6
郭云亮 <1060950677@qq.com>
2020-06-08 17:24:16 +0800

    banner

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -51,8 +51,16 @@
           <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
       </div>
-      <div class="ads-box"></div>
-      <div class="banner"></div>
+      <div class="ads-box">
+        <a v-bind:href="'/#/product' + item.id" v-for="(item, index) in adsList" v-bind:key = "index">
+          <img :src="item.img" alt="">
+        </a>
+      </div>
+      <div class="banner">
+        <a href="/#/product/30">
+          <img src="/imgs/banner-1.png" alt="">
+        </a>
+      </div>
       <div class="product-box"></div>
     </div>
     
@@ -140,6 +148,24 @@ import 'swiper/css/swiper.css'
             },
           ], 
           [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0],
+        ],
+        adsList: [
+          {
+            id: 33,
+            img: '/imgs/ads/ads-1.png'
+          },
+          {
+            id: 48,
+            img: '/imgs/ads/ads-2.jpg'
+          },
+          {
+            id: 45,
+            img: '/imgs/ads/ads-3.png'
+          },
+          {
+            id: 47,
+            img: '/imgs/ads/ads-4.jpg'
+          },
         ]
       }
     }
@@ -227,6 +253,18 @@ import 'swiper/css/swiper.css'
           height: 100%;
         }
       }
+    }
+    .ads-box {
+      @include flex();
+      margin-top: 14px;
+      margin-bottom: 31px;
+      a {
+        width: 296px;
+        height: 167px;
+      }
+    }
+    .banner {
+      margin-bottom: 50px;
     }
   }
 </style>
